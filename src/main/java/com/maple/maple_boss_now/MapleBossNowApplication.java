028a43e2f81f14f2e77fbe1fc2f646cdc7a92d21
yoonsejong <oxwk123@gmail.com>
2024-06-10 13:05:52 +0900

    maple boos now start

---
+++ src/main/java/com/maple/maple_boss_now/MapleBossNowApplication.java
@@ -0,0 +1,13 @@
+package com.maple.maple_boss_now;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleBossNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapleBossNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maple/maple_boss_now/MapleBossNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maple.maple_boss_now;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleBossNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
