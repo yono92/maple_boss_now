fc6635ee8464d82bb9afc46ea282fcdd20ed55d1
yoonsejong <oxwk123@gmail.com>
2024-07-19 02:04:16 +0900

    [style] 지원 종료 문법 수정 및 변경

--- src/main/java/com/maple/maple_boss_now/controller/SearchController.java
+++ src/main/java/com/maple/maple_boss_now/controller/SearchController.java
@@ -6,6 +6,7 @@ import com.maple.maple_boss_now.dto.guild.GuildInfoResponse;
 import com.maple.maple_boss_now.service.SearchService;
 import com.maple.maple_boss_now.service.guild.GuildService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
+@Slf4j
 public class SearchController {
 
     private final SearchService searchService;
@@ -25,8 +27,6 @@ public class SearchController {
     public ResponseEntity<CharacterDetailsResponse> getCharacterInfo(@RequestParam String characterName) {
         return ResponseEntity.ok(searchService.getCharacterDetailsByName(characterName));
     }
-
-
 
 
     @GetMapping("/guild")
