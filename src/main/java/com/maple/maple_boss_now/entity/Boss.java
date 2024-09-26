cbc60e530daff5f5fe02e87b22e79d0372ffc3f9
yoonsejong <oxwk123@gmail.com>
2024-09-26 22:52:40 +0900

    [refactor] 보스 class 수정

--- src/main/java/com/maple/maple_boss_now/entity/Boss.java
+++ src/main/java/com/maple/maple_boss_now/entity/Boss.java
@@ -6,21 +6,24 @@ import lombok.*;
 
 @Entity
 @Table(name = "bosses")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Boss {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // IDENTITY로 ID 자동 증가 설정
     private Long id;
 
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(20)")
     private BossDifficulty difficulty;
 
     @Column(nullable = false)
     private String category; // 일일보스, 주간보스, 월간보스 구분
+
 }
